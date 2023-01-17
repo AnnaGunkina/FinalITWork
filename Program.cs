d9f9c01aa0d1125bf9b990f3075b2512450bbb1d
Anna Gunkina <gunkinaaa@mail.ru>
2023-01-18 01:22:23 +0300

    Добавили Program.cs

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿Console.Clear();
+string[] ArrayStrings = new string[] { };
+Console.Write("Введите количество элементов массива - строк: ");
+int n = int.Parse(Console.ReadLine());
+ArrayStrings = new string[n];
+for (int i = 0; i < ArrayStrings.Length; i++)
+{
+    Console.Write($" Введите {i + 1} строку: ");
+    ArrayStrings[i] = Console.ReadLine();
+}
+Console.WriteLine();
+Console.Write("Массив введенных данных: ");
+Console.Write($"{PrintArray(ArrayStrings)}");
+Console.WriteLine();
+int length = 3;  
+
+int CheckArray(string[] array, int length)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= length) result++;
+    }
+    return result;
+}
+
+int numbers = CheckArray(ArrayStrings, length); 
+
+string[] newArrayStrings = new string[numbers]; 
+NewArray(ArrayStrings, newArrayStrings, length);
+
+void NewArray(string[] oldArray, string[] newArray, int lengthLimit)
+{
+    int temp = 0;
+    for (int i = 0; i < oldArray.Length; i++)
+    {
+        if (oldArray[i].Length <= lengthLimit)
+        {
+            newArray[temp] = oldArray[i];
+            temp++;
+        }
+    }
+}
+
+string PrintArray(string[] array)
+{
+    string result = string.Empty;
+    result = "[ ";
+    for (int i = 0; i < array.Length; i++)
+    {
+        result += $"{array[i],1}";
+        if (i < array.Length - 1) result += ", ";
+    }
+    result += " ]";
+    return result;
+}
+
+Console.Write("Новый массив из строк, длина которых меньше, либо равна 3 символам -> ");
+Console.Write($"{PrintArray(newArrayStrings)}");
+
